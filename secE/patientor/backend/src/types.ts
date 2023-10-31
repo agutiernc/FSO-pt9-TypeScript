90d23c8201cb35c8b8c3c99afa621b7500566f2a
Alfonso Gutierrez <agutiernc@gmail.com>
2023-10-30 20:40:13 -0700

    add new types and did ex 9.22

--- secE/patientor/backend/src/types.ts
+++ secE/patientor/backend/src/types.ts
@@ -4,13 +4,63 @@ export interface Diagnose {
   latin?: string;
 }
 
-// Entry added for ex 9.20
-export interface Entry {
+
+// Added during Section E: Full Entries
+interface BaseEntry {
+  id: string;
   description: string;
   date: string;
-  code: string;
   specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+  // diagnosisCodes?: Diagnose['code'][]; // Can also do it this way
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+
+// Entry added for ex 9.20
+// export interface Entry {
+//   description: string;
+//   date: string;
+//   code: string;
+//   specialist: string;
+// }
 
 export interface Patient {
   id: string;
@@ -33,3 +83,43 @@ export enum Gender {
   M = 'male',
   W = 'female'
 }
+
+
+
+// ================  Before Section E: Full Entries  ====================
+
+// export interface Diagnose {
+//   code: string;
+//   name: string;
+//   latin?: string;
+// }
+
+// // Entry added for ex 9.20
+// export interface Entry {
+//   description: string;
+//   date: string;
+//   code: string;
+//   specialist: string;
+// }
+
+// export interface Patient {
+//   id: string;
+//   name: string;
+//   dateOfBirth: string;
+//   ssn: string;
+//   gender: string;
+//   occupation: string;
+//   entries: Entry[]; // added for ex 9.20
+// }
+
+// // export type NonSensitivePatientInfo = Omit<Patient, 'ssn'>;
+
+// // added for ex 9.20
+// export type NonSensitivePatientInfo = Omit<Patient, 'ssn' | 'entries'>;
+
+// export type NewPatientEntry = Omit<Patient, 'id'>;
+
+// export enum Gender {
+//   M = 'male',
+//   W = 'female'
+// }
